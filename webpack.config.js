e44dd38b8db4f0b97f613a7a7638eb0b77e40b49
alf130386 <35611821+alf130386@users.noreply.github.com>
2022-03-07 16:16:22 +0200

    add project files

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: [
+        './src/main.js'
+    ],
+    output: {
+        filename: 'build.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+};
